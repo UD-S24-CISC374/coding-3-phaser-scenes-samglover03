24e1faf6e6f271e1a69fe068716b55c1d8217117
sam <sjglover@udel.edu>
2024-03-06 19:18:39 -0500

    Added character sprite and added animation for left and right movement

--- src/scenes/mainScene.ts
+++ src/scenes/mainScene.ts
@@ -1,9 +1,8 @@
 import Phaser from "phaser";
-import PhaserLogo from "../objects/phaserLogo";
-import FpsText from "../objects/fpsText";
 
 export default class MainScene extends Phaser.Scene {
-    fpsText: FpsText;
+    private player?: Phaser.Physics.Arcade.Sprite;
+    private cursors?: Phaser.Types.Input.Keyboard.CursorKeys;
 
     constructor() {
         super({ key: "MainScene" });
@@ -11,8 +10,40 @@ export default class MainScene extends Phaser.Scene {
 
     create() {
         this.add.image(650, 350, "dune").setScale(1.5, 1.5);
-        new PhaserLogo(this, this.cameras.main.width / 2, 0);
-        this.fpsText = new FpsText(this);
+
+        this.cursors = this.input.keyboard?.createCursorKeys();
+
+        this.player = this.physics.add
+            .sprite(650, 450, "character")
+            .setScale(0.25, 0.25);
+        this.player.setBounce(0.2);
+        this.player.setCollideWorldBounds(true);
+
+        this.anims.create({
+            key: "left",
+            frames: this.anims.generateFrameNumbers("character", {
+                start: 8,
+                end: 11,
+            }),
+            frameRate: 10,
+            repeat: -1,
+        });
+
+        this.anims.create({
+            key: "turn",
+            frames: [{ key: "character", frame: 1 }],
+            frameRate: 20,
+        });
+
+        this.anims.create({
+            key: "right", 
+            frames: this.anims.generateFrameNumbers("character", {
+                start: 4, 
+                end: 7,
+            }),
+            frameRate: 10,
+            repeat: -1,
+        })
 
         const message = `Phaser v${Phaser.VERSION}`;
         this.add
@@ -24,6 +55,16 @@ export default class MainScene extends Phaser.Scene {
     }
 
     update() {
-        this.fpsText.update();
+        if (this.cursors?.left.isDown) {
+            this.player?.setVelocityX(-160);
+            this.player?.anims.play("left", true);
+        }
+        else if (this.cursors?.right.isDown) {
+            this.player?.setVelocityX(160);
+            this.player?.anims.play("right", true);
+        } else {
+            this.player?.setVelocityX(0);
+            this.player?.anims.play("turn");
+        }
     }
 }
--- src/scenes/preloadScene.ts
+++ src/scenes/preloadScene.ts
@@ -7,7 +7,11 @@ export default class PreloadScene extends Phaser.Scene {
 
     preload() {
         this.load.image("dune", "assets/dune.jpg");
-    }
+        this.load.spritesheet("character", "assets/character.png", {
+            frameWidth: 383.5,
+            frameHeight: 500,
+        });
+    }   
 
     create() {
         this.scene.start("MainScene");
